d60c464b6b7805b318c56dbc9a9f5a4e21371e45
Vector <leopold.koz@gmail.com>
2018-04-29 15:04:23 +0200

    client: Added dbup.sql

---
+++ client/src/main/sql/dbup.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS settings (
+    row_guard INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(32) NOT NULL,
+    public_key BLOB(32) NOT NULL,
+    private_key BLOB(32) NOT NULL,
+    check(row_guard = 0)
+);
+
+CREATE TABLE IF NOT EXISTS ephemeral_keys (
+    id_key INTEGER PRIMARY KEY AUTOINCREMENT,
+    public_key BLOB(32) NOT NULL,
+    private_key BLOB(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(32) NOT NULL,
+    public_key BLOB(32) NOT NULL,
+    is_verified INTEGER NOT NULL,
+    out_sequence INTEGER NOT NULL,
+    in_sequence INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    id_friend INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_user INTEGER NOT NULL REFERENCES users(id_user)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id_message INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_from INTEGER NOT NULL REFERENCES users(id_user),
+    message VARCHAR(8196) NOT NULL
+);

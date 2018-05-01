3f2c90ef7eb02f4ddcc2a1579eb675733b7feba1
Teapot418 <teapot404@gmail.com>
2018-05-01 18:25:04 +0200

    client-app, server: Test apps

--- client-app/src/main/java/com/github/connteam/conn/client/app/App.java
+++ client-app/src/main/java/com/github/connteam/conn/client/app/App.java
@@ -1,7 +1,28 @@
 package com.github.connteam.conn.client.app;
 
+import java.io.IOException;
+
+import com.github.connteam.conn.client.net.NetClient;
+import com.github.connteam.conn.client.net.NetClientHandler;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("client");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Connecting");
+        NetClient client = NetClient.connect("127.0.0.1", 9090);
+
+        client.setHandler(new NetClientHandler() {
+            @Override
+            public void onLogin() {
+                System.out.println("Logged in!");
+            }
+        
+            @Override
+            public void onDisconnect(IOException err) {
+                System.out.println("Disconnected: " + err);
+            }
+        });
+
+        System.out.println("Authenticating");
+        client.login("admin123");
     }
 }
--- server/src/main/java/com/github/connteam/conn/server/App.java
+++ server/src/main/java/com/github/connteam/conn/server/App.java
@@ -1,7 +1,41 @@
 package com.github.connteam.conn.server;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.github.connteam.conn.server.net.NetServer;
+import com.github.connteam.conn.server.net.NetServerClient;
+import com.github.connteam.conn.server.net.NetServerClientHandler;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("server");
+    static AtomicInteger count = new AtomicInteger();
+
+    public static void main(String[] args) throws IOException {
+        try (NetServer server = NetServer.listen(9090)) {
+            System.out.println("Listening");
+            while (true) {
+                handle(server.accept());
+            }
+        }
+    }
+
+    public static void handle(NetServerClient client) {
+        int id = count.incrementAndGet();
+        System.out.println(id + " connected");
+
+        client.setHandler(new NetServerClientHandler(){
+            @Override
+            public boolean onLogin(String username) {
+                System.out.println(id + " authenticated: " + username);
+                return true;
+            }
+        
+            @Override
+            public void onDisconnect(IOException err) {
+                System.out.println(id + " disconnected: " + err);
+            }
+        });
+
+        client.handle();
     }
 }

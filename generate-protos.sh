7279ea418a4496a35167b1f0839c75760ac41dfb
Teapot418 <teapot404@gmail.com>
2018-05-03 23:18:30 +0200

    Travis fix - trilogy

--- generate-protos.sh
+++ generate-protos.sh
@@ -5,5 +5,5 @@ set -e
 PROTO_DIR=core/src/main/java/com/github/connteam/conn/core/net/proto/
 
 rm -fr "$PROTO_DIR"
-mvn com.github.os72:protoc-jar-maven-plugin:run
+mvn -f "core/pom.xml" com.github.os72:protoc-jar-maven-plugin:run
 find "$PROTO_DIR" -name '*.java' | xargs sed -i -r 's/public final class /@SuppressWarnings("all")\n\0/g'

30b14a2a7d1678a5c0e57db9e8e5f44ee634413b
Teapot418 <teapot404@gmail.com>
2018-05-03 20:46:55 +0200

    Logging settings

--- start-client.sh
+++ start-client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ROOT=`pwd`
 java -Djavax.net.ssl.trustStore=conn_keystore -Djavax.net.ssl.trustStorePassword=password \
+     -Djava.util.logging.config.file=logging.properties                                   \
      -jar "$ROOT/client-app/target/client-app-1.0-SNAPSHOT.jar"

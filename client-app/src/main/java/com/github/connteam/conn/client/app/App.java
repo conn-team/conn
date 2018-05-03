cddff2933affa2bdf4c73bc3f56b70b26a86729f
Teapot418 <teapot404@gmail.com>
2018-05-04 01:00:56 +0200

    Fix mixed tabs again

--- client-app/src/main/java/com/github/connteam/conn/client/app/App.java
+++ client-app/src/main/java/com/github/connteam/conn/client/app/App.java
@@ -19,14 +19,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
-	private final static Logger LOG = LoggerFactory.getLogger(App.class);
+    private final static Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws IOException {
-		final Settings settings = new Settings();
+        final Settings settings = new Settings();
 
-		settings.setUsername("teapot");
-		settings.setPublicKey(Base64.getDecoder().decode("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEE0+P3PrmpY+o605eYFdh1xD/TzL5QBf862D4CHejNidTPbeLcxGFq4BEjDxOFhtB6WtOYa57B24F2XU/wb9z6Q=="));
-		settings.setPrivateKey(Base64.getDecoder().decode("MEACAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJjAkAgEBBB9hQeQlV1JcELdUlk44FHY9zkPl92lG/PUdndyVlFpB"));
+        settings.setUsername("teapot");
+        settings.setPublicKey(Base64.getDecoder().decode("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEE0+P3PrmpY+o605eYFdh1xD/TzL5QBf862D4CHejNidTPbeLcxGFq4BEjDxOFhtB6WtOYa57B24F2XU/wb9z6Q=="));
+        settings.setPrivateKey(Base64.getDecoder().decode("MEACAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJjAkAgEBBB9hQeQlV1JcELdUlk44FHY9zkPl92lG/PUdndyVlFpB"));
 
         DataProvider provider = new DataProvider() {
             @Override
@@ -34,129 +34,129 @@ public class App {
                 return Optional.of(settings);
             }
 
-			@Override
-			public List<EphemeralKey> getEphemeralKeys() {
-				return null;
-			}
+            @Override
+            public List<EphemeralKey> getEphemeralKeys() {
+                return null;
+            }
 
-			@Override
-			public Optional<EphemeralKey> getEphemeralKey(int id) {
-				return null;
-			}
+            @Override
+            public Optional<EphemeralKey> getEphemeralKey(int id) {
+                return null;
+            }
 
-			@Override
-			public int insertEphemeralKey(EphemeralKey key) {
-				return 0;
-			}
+            @Override
+            public int insertEphemeralKey(EphemeralKey key) {
+                return 0;
+            }
 
-			@Override
-			public boolean deleteEphemeralKey(int id) {
-				return false;
-			}
+            @Override
+            public boolean deleteEphemeralKey(int id) {
+                return false;
+            }
 
-			@Override
-			public Optional<EphemeralKey> popEphemeralKey() {
-				return null;
-			}
+            @Override
+            public Optional<EphemeralKey> popEphemeralKey() {
+                return null;
+            }
 
-			@Override
-			public List<Friend> getFriends() {
-				return null;
-			}
+            @Override
+            public List<Friend> getFriends() {
+                return null;
+            }
 
-			@Override
-			public Optional<Friend> getFriendById(int id) {
-				return null;
-			}
+            @Override
+            public Optional<Friend> getFriendById(int id) {
+                return null;
+            }
 
-			@Override
-			public Optional<Integer> insertFriend(Friend friend) {
-				return null;
-			}
+            @Override
+            public Optional<Integer> insertFriend(Friend friend) {
+                return null;
+            }
 
-			@Override
-			public boolean deleteFriend(int id) {
-				return false;
-			}
+            @Override
+            public boolean deleteFriend(int id) {
+                return false;
+            }
 
-			@Override
-			public List<Message> getMessageFrom(int idFrom) {
-				return null;
-			}
+            @Override
+            public List<Message> getMessageFrom(int idFrom) {
+                return null;
+            }
 
-			@Override
-			public List<Message> getMessageTo(int idFrom) {
-				return null;
-			}
+            @Override
+            public List<Message> getMessageTo(int idFrom) {
+                return null;
+            }
 
-			@Override
-			public Optional<Message> getMessage(int idMessage) {
-				return null;
-			}
+            @Override
+            public Optional<Message> getMessage(int idMessage) {
+                return null;
+            }
 
-			@Override
-			public int insertMessage(Message message) {
-				return 0;
-			}
+            @Override
+            public int insertMessage(Message message) {
+                return 0;
+            }
 
-			@Override
-			public boolean updateMessage(Message message) {
-				return false;
-			}
+            @Override
+            public boolean updateMessage(Message message) {
+                return false;
+            }
 
-			@Override
-			public int deleteMessageFrom(int idFrom) {
-				return 0;
-			}
+            @Override
+            public int deleteMessageFrom(int idFrom) {
+                return 0;
+            }
 
-			@Override
-			public int deleteMessagesTo(int idTo) {
-				return 0;
-			}
+            @Override
+            public int deleteMessagesTo(int idTo) {
+                return 0;
+            }
 
-			@Override
-			public boolean setSettings(Settings settings) {
-				return false;
-			}
+            @Override
+            public boolean setSettings(Settings settings) {
+                return false;
+            }
 
-			@Override
-			public Optional<User> getUser(int id) {
-				return null;
-			}
+            @Override
+            public Optional<User> getUser(int id) {
+                return null;
+            }
 
-			@Override
-			public Optional<User> getUserByUsername(String username) {
-				return null;
-			}
+            @Override
+            public Optional<User> getUserByUsername(String username) {
+                return null;
+            }
 
-			@Override
-			public List<User> getUsers() {
-				return null;
-			}
+            @Override
+            public List<User> getUsers() {
+                return null;
+            }
 
-			@Override
-			public List<User> getVerifiedUsers() {
-				return null;
-			}
+            @Override
+            public List<User> getVerifiedUsers() {
+                return null;
+            }
 
-			@Override
-			public int insertUser(User user) {
-				return 0;
-			}
+            @Override
+            public int insertUser(User user) {
+                return 0;
+            }
 
-			@Override
-			public boolean deleteUser(int id) {
-				return false;
-			}
+            @Override
+            public boolean deleteUser(int id) {
+                return false;
+            }
 
-			@Override
-			public boolean deleteUserByUsername(String username) {
-				return false;
-			}
+            @Override
+            public boolean deleteUserByUsername(String username) {
+                return false;
+            }
 
-			@Override
-			public void close() throws Exception {	
-			}
+            @Override
+            public void close() throws Exception {	
+            }
         };
 
         LOG.info("Connecting");
--- server/src/main/java/com/github/connteam/conn/server/app/App.java
+++ server/src/main/java/com/github/connteam/conn/server/app/App.java
@@ -30,134 +30,134 @@ public class App {
         user.setSignupTime(new Timestamp(System.currentTimeMillis()));
 
         DataProvider provider = new DataProvider() {
-			@Override
-			public Optional<EphemeralKey> getEphemeralKey(int keyId) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public Optional<EphemeralKey> getEphemeralKey(int keyId) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public List<EphemeralKey> getEphemeralKeyByUserId(int userId) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public List<EphemeralKey> getEphemeralKeyByUserId(int userId) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public int insertEphemeralKey(EphemeralKey key) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int insertEphemeralKey(EphemeralKey key) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public boolean updateEphemeralKey(EphemeralKey key) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean updateEphemeralKey(EphemeralKey key) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean deleteEphemeralKey(int keyId) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean deleteEphemeralKey(int keyId) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public int deleteEphemeralKeyByUserId(int userId) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int deleteEphemeralKeyByUserId(int userId) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public Optional<Message> getMessage(int idMessage) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public Optional<Message> getMessage(int idMessage) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public List<Message> getMessagesFrom(int idFrom) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public List<Message> getMessagesFrom(int idFrom) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public List<Message> getMessagesTo(int idTo) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public List<Message> getMessagesTo(int idTo) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public int insertMessage(Message message) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int insertMessage(Message message) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public boolean updateMessage(Message message) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean updateMessage(Message message) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean deleteMessage(int idMessage) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean deleteMessage(int idMessage) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public int deleteMessagesFrom(int idFrom) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int deleteMessagesFrom(int idFrom) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public int deleteMessagesTo(int idTo) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int deleteMessagesTo(int idTo) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public List<Observed> getObserved(int idObserver) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public List<Observed> getObserved(int idObserver) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public List<Observed> getObservers(int idObserved) throws DatabaseException {
-				return null;
-			}
+            @Override
+            public List<Observed> getObservers(int idObserved) throws DatabaseException {
+                return null;
+            }
 
-			@Override
-			public boolean insertObserved(Observed observed) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean insertObserved(Observed observed) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean deleteObserved(Observed observed) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean deleteObserved(Observed observed) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public Optional<User> getUser(int id) throws DatabaseException {
-				return Optional.ofNullable(user.getId() == id ? user : null);
-			}
+            @Override
+            public Optional<User> getUser(int id) throws DatabaseException {
+                return Optional.ofNullable(user.getId() == id ? user : null);
+            }
 
-			@Override
-			public Optional<User> getUserByUsername(String username) throws DatabaseException {
+            @Override
+            public Optional<User> getUserByUsername(String username) throws DatabaseException {
                 return Optional.ofNullable(user.getUsername().equals(username) ? user : null);
-			}
+            }
 
-			@Override
-			public int insertUser(User user) throws DatabaseException {
-				return 0;
-			}
+            @Override
+            public int insertUser(User user) throws DatabaseException {
+                return 0;
+            }
 
-			@Override
-			public boolean updateUser(User user) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean updateUser(User user) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean updateUserByUsername(User username) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean updateUserByUsername(User username) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean deleteUser(int id) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean deleteUser(int id) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public boolean deleteUserByUsername(String username) throws DatabaseException {
-				return false;
-			}
+            @Override
+            public boolean deleteUserByUsername(String username) throws DatabaseException {
+                return false;
+            }
 
-			@Override
-			public void close() throws Exception {
-			}
+            @Override
+            public void close() throws Exception {
+            }
         };
 
         try (ConnServer server = ConnServer.builder().setPort(9090).setTransport(Transport.SSL)

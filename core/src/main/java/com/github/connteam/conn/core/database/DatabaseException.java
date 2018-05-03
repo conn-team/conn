42891a908795a8dd9ae27bd3ad7cce90fa08841b
Vector <leopold.koz@gmail.com>
2018-05-03 17:28:01 +0200

    core: implemented all constructors for DatabaseException

--- core/src/main/java/com/github/connteam/conn/core/database/DatabaseException.java
+++ core/src/main/java/com/github/connteam/conn/core/database/DatabaseException.java
@@ -1,13 +1,19 @@
 package com.github.connteam.conn.core.database;
 
 public class DatabaseException extends Exception {
-    private static final long serialVersionUID = -3645623781051859120L;
-
     public DatabaseException() {
         super();
     }
 
-    public DatabaseException(String message) {
-        super(message);
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+    public DatabaseException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+
+    public DatabaseException(Throwable throwable) {
+        super(throwable);
     }
 }

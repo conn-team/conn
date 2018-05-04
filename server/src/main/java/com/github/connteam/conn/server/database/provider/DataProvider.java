5a1e62f77584a5e744db5cc85322fea79c7eaf53
teapotd <teapot404@gmail.com>
2018-05-04 23:13:36 +0200

    server/database: Precise close() exception

--- server/src/main/java/com/github/connteam/conn/server/database/provider/DataProvider.java
+++ server/src/main/java/com/github/connteam/conn/server/database/provider/DataProvider.java
@@ -1,5 +1,8 @@
 package com.github.connteam.conn.server.database.provider;
 
+import com.github.connteam.conn.core.database.DatabaseException;
+
 public interface DataProvider
         extends EphemeralKeyProvider, MessageProvider, ObservedProvider, UserProvider, AutoCloseable {
+    void close() throws DatabaseException;
 }

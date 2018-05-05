6268c7da8a1f6d7f40f1d0b3e101bc9c123caa70
teapotd <teapot404@gmail.com>
2018-05-05 11:24:33 +0200

    core/database: executeScriptFromResource silly implementation

---
+++ core/src/main/java/com/github/connteam/conn/core/database/DatabaseUtil.java
@@ -0,0 +1,35 @@
+package com.github.connteam.conn.core.database;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.github.connteam.conn.core.io.IOUtils;
+
+public final class DatabaseUtil {
+    private DatabaseUtil() {
+    }
+
+    public static void executeScriptFromResource(Connection conn, Class<?> clazz, String resourceName)
+            throws SQLException, IOException {
+        byte[] bytes = IOUtils.readAllBytes(clazz.getClassLoader().getResourceAsStream(resourceName));
+        if (bytes == null) {
+            throw new IOException("Resource not found");
+        }
+
+        final String SQLString = new String(bytes, StandardCharsets.UTF_8);
+
+        try (Statement stmt = conn.createStatement()) {
+            // Yes, it's bad, but we're executing trusted script, maybe rewrite it to
+            // something better later
+            for (String sql : SQLString.split(";")) {
+                sql = sql.trim();
+                if (sql.length() > 0) {
+                    stmt.execute(sql);
+                }
+            }
+        }
+    }
+}

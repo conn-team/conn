1fce82fabd7b6f54fd69d01d0f09cce87d58412c
teapotd <teapot404@gmail.com>
2018-05-06 23:08:37 +0200

    client-app: Login screen

--- client-app/src/main/java/com/github/connteam/conn/client/app/App.java
+++ client-app/src/main/java/com/github/connteam/conn/client/app/App.java
@@ -1,10 +1,25 @@
 package com.github.connteam.conn.client.app;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("views/LoginView.fxml"));
+        primaryStage.setScene(new Scene(view));
+        primaryStage.setTitle("Conn");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) throws Exception {
         if (args.length > 0 && args[0].equalsIgnoreCase("-cli")) {
             new CLI().start();
-            return;
+        } else {
+            launch(App.class, args);
         }
     }
 }

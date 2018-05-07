c32242a9e380d0ca8c7e44bcd0cc9b686a292f71
teapotd <teapot404@gmail.com>
2018-05-07 22:16:56 +0200

    client-app: Username filtering in register view

--- client-app/src/main/java/com/github/connteam/conn/client/app/controllers/RegisterViewController.java
+++ client-app/src/main/java/com/github/connteam/conn/client/app/controllers/RegisterViewController.java
@@ -1,19 +1,48 @@
 package com.github.connteam.conn.client.app.controllers;
 
 import com.github.connteam.conn.client.app.App;
+import com.github.connteam.conn.core.Sanitization;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ProgressBar;
+
 public class RegisterViewController {
     private final App app;
+
+    @FXML private ProgressBar progressBar;
+    @FXML private TextField usernameField;
+    @FXML private Button registerButton;
 
     public RegisterViewController(App app) {
         this.app = app;
     }
 
     @FXML
+    public void initialize() {
+        registerButton.setDisable(true);
+
+        usernameField.textProperty().addListener((prop, old, cur) -> {
+            registerButton.setDisable(!Sanitization.isValidUsername(cur));
+            
+            String filtered = Sanitization.filterUsername(cur);
+            if (!filtered.equals(cur)) {
+                usernameField.setText(filtered);
+            }
+        });
+    }
+
+    @FXML
     protected void onRegister(ActionEvent event) {
+        String username = usernameField.getText();
+        if (!Sanitization.isValidUsername(username)) {
+            return;
+        }
+
+        // TODO
     }
 
     @FXML
--- core/src/main/java/com/github/connteam/conn/core/Sanitization.java
+++ core/src/main/java/com/github/connteam/conn/core/Sanitization.java
@@ -3,11 +3,22 @@ package com.github.connteam.conn.core;
 import java.util.regex.Pattern;
 
 public final class Sanitization {
+    public static final int MIN_USERNAME_LENGTH = 4;
+    public static final int MAX_USERNAME_LENGTH = 32;
     private static final Pattern USERNAME_REGEXP = Pattern.compile("[a-zA-Z0-9_]{4,32}");
+    private static final Pattern USERNAME_FILTER = Pattern.compile("[^a-zA-Z0-9_]");
 
     private Sanitization() {}
 
     public static boolean isValidUsername(String username) {
         return USERNAME_REGEXP.matcher(username).matches();
+    }
+
+    public static String filterUsername(String username) {
+        username = USERNAME_FILTER.matcher(username).replaceAll("");
+        if (username.length() > MAX_USERNAME_LENGTH) {
+            username = username.substring(0, MAX_USERNAME_LENGTH);
+        }
+        return username;
     }
 }

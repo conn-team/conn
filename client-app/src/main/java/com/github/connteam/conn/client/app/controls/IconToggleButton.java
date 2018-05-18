e508fa0974e38e8bc38ab23fc0bb97e988643ea3
teapotd <teapot404@gmail.com>
2018-05-18 11:09:14 +0200

    client-app: IconToggleButton (untested)

---
+++ client-app/src/main/java/com/github/connteam/conn/client/app/controls/IconToggleButton.java
@@ -0,0 +1,30 @@
+package com.github.connteam.conn.client.app.controls;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.css.PseudoClass;
+import javafx.scene.input.MouseEvent;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public class IconToggleButton extends FontAwesomeIconView {
+    private final static PseudoClass SELECTED_PSEUDOCLASS = PseudoClass.getPseudoClass("selected");
+
+    private final BooleanProperty selected = new SimpleBooleanProperty();
+
+    public IconToggleButton() {
+        selected.addListener((prop, old, cur) -> pseudoClassStateChanged(SELECTED_PSEUDOCLASS, cur));
+        addEventHandler(MouseEvent.MOUSE_CLICKED, e -> setSelected(!isSelected()));
+    }
+
+    public boolean isSelected() {
+        return selected.get();
+    }
+
+    public void setSelected(boolean x) {
+        selected.set(x);
+    }
+
+    public BooleanProperty selectedProperty() {
+        return selected;
+    }
+}

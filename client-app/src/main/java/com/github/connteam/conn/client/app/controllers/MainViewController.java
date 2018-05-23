00c52eccfbe57bc5bb6bed343a9c3af0ca8e0d6e
teapotd <teapot404@gmail.com>
2018-05-23 19:26:14 +0200

    client-app: Scroll conversations to bottom

--- client-app/src/main/java/com/github/connteam/conn/client/app/controllers/MainViewController.java
+++ client-app/src/main/java/com/github/connteam/conn/client/app/controllers/MainViewController.java
@@ -78,6 +78,8 @@ public class MainViewController {
             ctx.set(conversationUsernameLabel.textProperty(), cur.getUser().getUsername(), "");
             ctx.set(conversationFingerprintLabel.textProperty(),
                     CryptoUtil.getFingerprint(cur.getUser().getRawPublicKey()), "");
+
+            ctx.listen(cur.getMessages(), x -> messagesView.scrollTo(Integer.MAX_VALUE));
         }
 
         welcomeBox.setVisible(cur == null);
